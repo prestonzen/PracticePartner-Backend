9fa8bd2de1be87ff0bf8e13a2ac0300904768d8e
rafy <mohammadrafy1001@gmail.com>
2024-03-13 18:57:33 +0600

    image and text generation fixed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,7 +1,7 @@
 const OpenAI = require('openai');
 
 // Provide your OpenAI API key here
-const apiKey = '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';
+const apiKey = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI({
   apiKey: apiKey,
