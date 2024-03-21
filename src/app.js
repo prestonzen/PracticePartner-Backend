95ff17e32c8e597d2628df35196e48ae1963ad5d
Shuvro Hawk <shuvrohawk@gmail.com>
2024-03-22 02:23:56 +0600

    added middleware

--- src/app.js
+++ src/app.js
@@ -15,9 +15,15 @@ const chatRoutes = require('../routes/chatRoutes');
 // Middlewares
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://practicepartner.ai');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const corsOptions = {
-  origin: ['http://localhost:3001', 'https://practicepartner.kaizenapps.com', 'https://practicepartner.ai'], // Allow requests from this origin
+  origin: ['http://localhost:3001', 'https://practicepartner.kaizenapps.com', 'https://practicepartner.ai', 'https://married-dolls-cashiers-puts.trycloudflare.com'], // Allow requests from this origin
   methods: 'GET,POST', // Allow only GET and POST requests
   allowedHeaders: 'Content-Type,Authorization', // Allow only these headers
 };
