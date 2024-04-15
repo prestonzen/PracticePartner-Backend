bc1792e60762926a5fa185ad3a09a54a60afa617
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-04-15 21:08:51 +0600

    trying cors fix

--- src/app.js
+++ src/app.js
@@ -46,8 +46,10 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin',
+  '*',
   //  'https://practicepartner.ai'
-  'https://practice-partner-frontend-xi.vercel.app');
+  // 'https://practice-partner-frontend-xi.vercel.app'
+);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
