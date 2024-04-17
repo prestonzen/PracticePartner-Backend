94e2d7d40d5eb466a1006cfcbfb0a28f2b86e223
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-04-17 16:40:48 +0600

    trying cors fix

--- src/app.js
+++ src/app.js
@@ -44,16 +44,16 @@ const db = new Firestore({
 // Middlewares
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin',
-  '*',
-  //  'https://practicepartner.ai'
-  // 'https://practice-partner-frontend-xi.vercel.app'
-);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin',
+//   '*',
+//   //  'https://practicepartner.ai'
+//   // 'https://practice-partner-frontend-xi.vercel.app'
+// );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 const corsOptions = {
   credentials: true,
