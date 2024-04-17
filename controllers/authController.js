664f9fad8cc264def69c4d5f197ef76ee88c4e7a
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-04-17 20:53:44 +0600

    trying jwt fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const stripe = require('stripe')(
 );
 
 const jwt = require('jsonwebtoken');
-const cookieDomain = '.vercel.app';
+const cookieDomain = 'practice-partner-frontend-xi.vercel.app';
 
 exports.signup = async (req, res) => {
   try {
