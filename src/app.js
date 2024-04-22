732c51a89d016cba83309488589910a2b88fb739
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-04-22 23:22:35 +0600

    slight changes for vercel deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { sessionMiddleware } = require('../middlewares/sessionMiddleware');
 // const firebaseConfig = require('../config/firebase');
 const Firestore = require('@google-cloud/firestore');
 const errorMiddleware = require('../middlewares/errorMiddleware');
+require('dotenv').config();
 
 const stripe = require('stripe')(
   'sk_test_51OwpTS01Mx8CmgRTDrqwtjvL6AM18K1Pp2MYILW2d7P9Ebf3mMl9AdCFiDwoTEAx5NEqGJZhdHCtg9ayWTS8hN3l00tSitWqde'
